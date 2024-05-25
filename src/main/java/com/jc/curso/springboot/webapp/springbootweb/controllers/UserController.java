a89e0d072225dba280976693e321320d3d73369f
jcmartinez <jcmartinezcu@gmail.com>
2024-05-25 20:05:49 +0200

    video 13

--- src/main/java/com/jc/curso/springboot/webapp/springbootweb/controllers/UserController.java
+++ src/main/java/com/jc/curso/springboot/webapp/springbootweb/controllers/UserController.java
@@ -1,14 +1,18 @@
 package com.jc.curso.springboot.webapp.springbootweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
 @GetMapping("/details")
-public String details(){
-
+public String details(Model model ){
+    model.addAttribute("title", "Hola Mundo Spring Boot");
+    model.addAttribute("name", "Juan Carlos");
+    model.addAttribute("lastname", "Martínez");
+   
     return "details";
 }
 
