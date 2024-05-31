1f7ac7fd0993de4b4172b9b3fd84540b13057f43
jcmartinez <jcmartinezcu@gmail.com>
2024-05-31 17:34:46 +0200

    Listas en tymeleaf

--- src/main/java/com/jc/curso/springboot/webapp/springbootweb/controllers/UserController.java
+++ src/main/java/com/jc/curso/springboot/webapp/springbootweb/controllers/UserController.java
@@ -1,7 +1,11 @@
 package com.jc.curso.springboot.webapp.springbootweb.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.jc.curso.springboot.webapp.springbootweb.models.User;
@@ -18,6 +22,16 @@ public String details(Model model ){
     model.addAttribute("user", user);
    
     return "details";
+}
+
+@GetMapping("/list")
+public String list(ModelMap model){
+    List<User> users = new ArrayList<>();
+
+    model.addAttribute("users", users);
+    model.addAttribute("title", "Listado de usuarios!");
+
+    return "list";
 }
 
 }
