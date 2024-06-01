959d0555b2b0c3766d793b2c21f44799c50d51fd
jcmartinez <jcmartinezcu@gmail.com>
2024-06-01 15:59:31 +0200

    varios parametro pathvariable

--- src/main/java/com/jc/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
+++ src/main/java/com/jc/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.jc.curso.springboot.webapp.springbootweb.models.dto.ParamDto;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -20,6 +23,17 @@ public class PathVariableController {
         ParamDto param = new ParamDto();
         param.setMessage(message);
         return param;
+    }
+
+
+    @GetMapping("/mix/{product}/{id}")
+    public Map<String, Object> mixPathVar(@PathVariable String product, @PathVariable Long id){
+
+        Map<String, Object> json = new HashMap<>();
+        json.put("product", product);
+        json.put("id", id);
+
+        return json;
     }
     
 
