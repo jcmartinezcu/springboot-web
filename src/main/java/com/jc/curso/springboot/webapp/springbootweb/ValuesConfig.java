f1defab9f59b94f33923268d6951b7bd21ca33d5
jcmartinez <jcmartinezcu@gmail.com>
2024-06-01 16:45:18 +0200

    agregando archivos properties

---
+++ src/main/java/com/jc/curso/springboot/webapp/springbootweb/ValuesConfig.java
@@ -0,0 +1,15 @@
+package com.jc.curso.springboot.webapp.springbootweb;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+	@PropertySource("classpath:values.properties")
+
+})
+public class ValuesConfig {
+
+}
