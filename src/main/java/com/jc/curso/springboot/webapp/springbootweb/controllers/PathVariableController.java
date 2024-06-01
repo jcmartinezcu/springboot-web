a94c4307d7cda2f46002595f1d9ac662ed735972
jcmartinez <jcmartinezcu@gmail.com>
2024-06-01 15:54:08 +0200

    anotacion PathVariable

---
+++ src/main/java/com/jc/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
@@ -0,0 +1,26 @@
+package com.jc.curso.springboot.webapp.springbootweb.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jc.curso.springboot.webapp.springbootweb.models.dto.ParamDto;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+@RestController
+@RequestMapping("/api/var")
+public class PathVariableController {
+
+    @GetMapping("/baz/{message}")
+    public ParamDto baz(@PathVariable String message) {
+
+        ParamDto param = new ParamDto();
+        param.setMessage(message);
+        return param;
+    }
+    
+
+}
